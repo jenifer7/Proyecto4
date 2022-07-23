d94f858918241fda70b1e033ead639db6546be75
jenifer9 <jeni9jacinto7p@gmail.com>
2022-07-22 20:35:19 -0600

    obtener datos de api

--- js/apic.js
+++ js/apic.js
@@ -1,5 +1,5 @@
 let ul = document.getElementById("pokemons");
-let url = 'https://randomuser.me/api/?results=10';
+let url = 'https://pokeapi.co/api/v2/pokemon?limit=8';
 
 function createNode(element) {
     return document.createElement(element);
@@ -13,17 +13,19 @@ fetch(url)
     .then((resp) => resp.json())
 
     .then(function (data) {
-        let poke = data.results;
+        let poke = data;
+        console.log(poke);
 
         return poke.map(function (pokemon) {
             let li = createNode('li'),
                 img = createNode('img'),
                 span = createNode('span');
-            img.src = pokemon.picture.medium;
-            span.innerHTML = `${pokemon.name.first} ${pokemon.name.last}`;
+            img.src = pokemon.sprites.back_shiny;
+            span.innerHTML = `${pokemon.name}`;
             append(li, img);
             append(li, span);
             append(ul, li);
+            console.log(pokemon);
         })
     })
 
